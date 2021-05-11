84586e01ee38f26f8c62cd8e4d7985ebecf6a264
hugh577 <83644711+hugh577@users.noreply.github.com>
2021-05-11 16:03:52 +0900

    Update read_graph_ind.py

--- read_graph_ind.py
+++ read_graph_ind.py
@@ -1,10 +1,9 @@
 import numpy as np
 
 f = open('graph_ind.txt', 'r')
-i = 0
 nn = []  # node_number
 gn = []  # graph_number
-imsi = []  # 각 그래프 넘버에 들어가는
+imsi = []  # 각 그래프 넘버에 들어가는 노드들
 
 line = f.readline()
 item = line.split(" ")
@@ -13,7 +12,9 @@ gn = item[1].rstrip('\n')
 
 while True:
     line = f.readline()
-    if not line : break
+    if not line:
+        nn.append(imsi)
+        break
     item = line.split(" ")
     gn2 = item[1].rstrip('\n')
     if gn != gn2:
@@ -21,6 +22,4 @@ while True:
         imsi = []
     imsi.append(item[0].rstrip(','))
     gn = gn2
-
-print(nn[1])
 
